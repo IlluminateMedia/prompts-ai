77ffe6fd40e73a5a7ff6c3203c9966cae099c350
zemin <likeredapple@outlook.com>
2021-08-24 18:22:47 -0400

    – update a workspace model

--- backend/restapi/models/workspace.py
+++ backend/restapi/models/workspace.py
@@ -6,12 +6,12 @@ from .custom_model import CustomModel
 class Workspace(models.Model):
     name = models.CharField(max_length=255, blank=False)
     custom_model = models.ForeignKey(CustomModel, related_name="workspaces", on_delete=models.CASCADE, blank=True)
-    temperature = models.DecimalField(help_text=_("Temperature"), max_digits=5, decimal_places=2, default=0.00)
-    max_tokens = models.DecimalField(help_text=_("Max Tokens"), max_digits=5, decimal_places=2, default=0.00)
-    top_p = models.DecimalField(help_text=_("Top P"), max_digits=5, decimal_places=2, default=0.00)
-    n = models.IntegerField(default=1)
-    presence_penalty = models.DecimalField(help_text=_("Presence Penalty"), max_digits=5, decimal_places=2, default=0.00)
-    frequency_penalty = models.DecimalField(help_text=_("Frequency Penalty"), max_digits=5, decimal_places=2, default=0.00)
+    temperature = models.CharField(max_length=255, blank=False)
+    max_tokens = models.CharField(max_length=255, blank=False)
+    top_p = models.CharField(max_length=255, blank=False)
+    n = models.CharField(max_length=255, blank=False)
+    presence_penalty = models.CharField(max_length=255, blank=False)
+    frequency_penalty = models.CharField(max_length=255, blank=False)
     stop_symbols = models.JSONField(default=dict)
     prompt = models.TextField(blank=True, null=True)
 
